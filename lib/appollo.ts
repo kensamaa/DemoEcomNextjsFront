d542adc7e644e039e64ea8c14a260ecf6c97cff9
kensamaa <amineab203@gmail.com>
2023-12-03 23:22:43 +0100

    removing package

--- lib/appollo.ts
+++ lib/appollo.ts
@@ -1,8 +1,9 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
+//import { ApolloClient, InMemoryCache } from "@apollo/client";
+/*
 const AppoClient = new ApolloClient({
   uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
   cache: new InMemoryCache(),
 });
 
 export default AppoClient;
+*/
